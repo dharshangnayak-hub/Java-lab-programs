76f40f9a30b86dd1592d8507dd7b5d07661f18e8
dharshangnayak-hub <dharshangnayak@gmail.com>
2025-11-13 12:01:35 +0530

    Add files via upload

---
+++ quadraticequation.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+public class quadraticequation {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter coefficient a: ");
+        double a = scanner.nextDouble();
+
+        System.out.print("Enter coefficient b: ");
+        double b = scanner.nextDouble();  
+        System.out.print("Enter coefficient c: ");
+        double c = scanner.nextDouble();
+        double discriminant = b * b - 4 * a * c;
+        if(a==0){
+            System.out.println("Linear equation");
+            double root=-c/b;
+            System.out.println("Root"+root);
+        }
+        else if (a==0 && b==0){
+            System.out.println("Not an eauation");
+        }
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Roots are real and different.");
+            System.out.println("Root 1: " + root1);
+            System.out.println("Root 2: " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("Roots are real and the same.");
+            System.out.println("Root: " + root);
+        } else {
+            System.out.println("Roots are complex and different.");
+            double realPart = -b / (2 * a);
+            double imaginaryPart = Math.sqrt(-discriminant) / (2 * a);
+            System.out.println("Root 1: " + realPart + " + " + imaginaryPart + "i");
+            System.out.println("Root 2: " + realPart + " - " + imaginaryPart + "i");
+        }
+        scanner.close();
+    }
+}
